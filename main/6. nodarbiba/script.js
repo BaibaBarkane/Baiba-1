eca9fcd771f1179227bbd6205cb1b80e9e2a19e3
BaibaBarkane <baiba.barkane@gmail.com>
2022-05-22 22:06:40 +0300

    6md

--- main/6. nodarbiba/script.js
+++ main/6. nodarbiba/script.js
@@ -7,30 +7,47 @@
         Mums piejajams masivs ar burtiem
         Lietotajam ir iespeja ievadit skaitli 
         Ja ievaditais skaitlis atbilst burtu masivam tad šis burts tiek izvadits uz ekranu 
-        Spiežot pogas uz tastaturas aplikacija parbauda ievadito vertibu ar izveleto burtu un pažiņo par rezultatu
+        Spiežot pogas uz tastaturas aplikacija parbauda ievadito vertibu ar 
+        izveleto burtu un pažiņo par rezultatu
 */
 
 
 // Implementacija
 // 1) izvejdot massivu ar burtiem characters[]
+const characters = ["z", "a", "l", "k", "t", "i", "s"];
 
 // 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
+let arrayId;
 
-// 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts (getElementById) 
+// 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts(getElementById) 
 
+let output = getElementById;
 /* 4) izvejtot funkciju getValue() funkcijas ipašibas 
         neko nesaņem
         izmantojot prompt() medoti --> https://www.w3schools.com/jsref/met_win_prompt.asp piesaistit vertibu mainigam arrayId
         izveidot parbaudi if else un parliecinaties ka ievietotais skatlis pieder intervalam no 0 lidz characters.lenght
         ja gadijuma izsaukt finkciju print value (5 punkts)
         ne gadijuma izvadit kļudas paziņojomu pec tam izsaukt funkciju getValue velreizi
-*/      
+        */
+function getValue() {
+        arrayId = prompt("Ievadiet burtu")
+        if (arrayId = < characters.length) {
+                printValue();
+        } else {
+        getValue();
+                }
+        }
+                
+       
 
 /* 5) izvejtot funkciju printValues() funkcijas ipašibas
         neko nesaņem
         izmaiņam defineto output mainigo iekšejo text (innerText) ar izveleto burtu masiva (characters[arrayId])
 */
-
+function printValue() {
+        console.log(output);
+        
+}
 // 6) izsaucam funkciju getValue()
 
 /* 7) japievino event listener keyPress un parbaudit ievadito to tastaturas vertibu ar izveleto vertibu pirm tam

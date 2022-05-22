8f2501f5e515e5d791815d8e691c774739d392db
BaibaBarkane <baiba.barkane@gmail.com>
2022-05-22 18:38:31 +0300

    6md

---
+++ main/6. nodarbiba/Cikls.js
@@ -0,0 +1,14 @@
+const numberArray = [1, 2, 3, 4, 5];
+
+const numberSum = [];
+
+for (let i = 0; i < numberArray.length; sum++) {
+  sum = sum + numberArray[i];
+}
+
+// for
+
+// forEach
+numberArray.forEach((number) => (sum += number));
+
+console.log(sum); // 15
---
+++ main/6. nodarbiba/Tests.js
@@ -0,0 +1,14 @@
+const nameArray = ["Vards", "2 Vards", "Uzvards"];
+
+let name = "";
+
+const concatStrings = (oldName, wordToAdd) => {
+    return "$"
+};
+
+
+nameArray.forEach(name) => {
+    
+};
+
+console.log(name); // "Varbs 2 Vards Uzvards"

921ff589e735db10bd7f27798a742a52d5c66e90
BaibaBarkane <baiba.barkane@gmail.com>
2022-05-31 21:44:33 +0300

    7md

--- main/7 nodarbiba/app.js
+++ main/7 nodarbiba/app.js
@@ -2,9 +2,15 @@
 // Nokopejiet šo mapi savas majas darba mape
 // Aplikacijas darbas principi var apskatit video appDemo.mp4
 
+let taskAdder;
+let taskList;
+let myTasksContainer;
 let taskInput = documentGetElementById("taskInput");
 let addBtn = documentGetElementById("addBtn");
-let taskList = documentGetElementById("taskList");
+
+taskAdder = document.querySelector(".taskAdder");
+myTasksContainer = documentGetElementById(".myTasks");
+taskList = JSON.parse(localStorage.getItem("taskList"));
 
 const saveToLocalStorage = () => {
   localStorage.setItem("taskList", JSON.stringify(taskList));
@@ -21,51 +27,41 @@ const addTask = () => {
   taskInput.value = "";
   taskList.push(task);
   saveToLocalStorage();
-    const renderTask() {
-      
-  }
+  const renderTask = () => {
+    localStorage.removeItem;
+  };
 };
 
-/* Mums nepieciešams izviedot nelielu todo aplikaciju ar iespeju pievienot izdzest un atziment izdarito notikumu
-    Mums nepieciešams 4 funkcijas
-        addTask() - pievienot notikumu
-        saveToLocalStorage() - saglaba notikumu
-      
-
-        renderTask() - izvada sarakstu notikumu
-            1 - jaizmanto innerHTML
-            2 - jaizmanto vienu no cikliem lai iziet cauri visam localstorage ierakstiem
-            3 - šis html palidzes izvadit datus
-            return `<li data-index='${i}'>
+const renderTask = () => {
+  letTaskToRender = [];
+  taskList.forEach((item, i) => {
+    let isTaskChecked = (item = done ? "checked" : "");
+    let task = "";
+    tasksToRender.push(task);
+    return `<li data-index='${i}'>
                         <div class="">
                             ${data.textTask}<span class="remove">❌</span>
                         </div>
                     </li>`;
-            4 - Gadijuma ja elements ir apziments ka izdarits mums nepiecišams pievienot klassi .done ../style.css
-                Lidz ar to nepieciešams izveidot parbaudi if else lai parbaudit test.done === 'true'
+  });
+  myTasksContainer.innerHTML = tasksToRender.join("");
 
-        toggleDone() - atzimet ka izdarito
-            Mums nepiecišams pievinot 2 eventListener
-                addEventListener('submit', addTask);   ----> nostradas kad mes nospiedam pievinot pogu un izsaucam funkciju addTask
-                addEventListener('click', toggleDone); ----> nostradas kad mes nospiedam uz elementu saraksta un izsaucam funkciju toggleDone
+  //       4 - Gadijuma ja elements ir apziments ka izdarits mums nepiecišams pievienot klassi .done ../style.css
+  //         Lidz ar to nepieciešams izveidot parbaudi if else lai parbaudit test.done === 'true'
 
-                funkcija toogleDone dara divas darbibas
-                gadijuma ja bija nospiesta izdžešanas poga, mes izdesam elementu no localStorage izsaucam renderTask funkciju lai atjaunto sarakstu
-                gadijuma ja bija nospiest elements mes nomainam elementam done vertibu done: false --> done: true un izsaucam renderTask funkciju lai atjauno sarakstu
-*/
-const taskAdder = document.querySelector(".taskAdder");
-const myTasks = document.querySelector(".myTasks");
-const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
+  //  toggleDone() - atzimet ka izdarito
+  //       Mums nepiecišams pievinot 2 eventListener
+  //          addEventListener('submit', addTask);   ----> nostradas kad mes nospiedam pievinot pogu un izsaucam funkciju addTask
+  //          addEventListener('click', toggleDone); ----> nostradas kad mes nospiedam uz elementu saraksta un izsaucam funkciju toggleDone
 
-// addEventListener click
-// addEventListener submit
+  /*             funkcija toogleDone dara divas darbibas
+                 gadijuma ja bija nospiesta izdžešanas poga, mes izdesam elementu no localStorage izsaucam renderTask funkciju lai atjaunto sarakstu
+                 gadijuma ja bija nospiest elements mes nomainam elementam done vertibu done: false --> done: true un izsaucam renderTask funkciju lai atjauno sarakstu
+ */
+  // addEventListener click
+  // addEventListener submit
 
-renderTask();
+  renderTask();
 
-function addTask() {}
-
-function saveToLocalStorage() {}
-
-function renderTask() {}
-
-function toggleDone(e) {}
+  function toggleDone(e) {}
+};

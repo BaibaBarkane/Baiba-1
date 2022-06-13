165df011c0198267ef1855b9dd8e568412eea3f0
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-13 20:45:21 +0300

    7md

--- main/7 nodarbiba/app.js
+++ main/7 nodarbiba/app.js
@@ -1,15 +1,15 @@
 // TODO app (dienas planotaju)
 
-let taskAdder = document.querySelector("taskAdder");
-let myTasksContainer = document.getElementById("myTasks");
+let taskAdder = document.querySelector(".taskAdder");
+let myTasksContainer = document.getElementById(".myTasks");
 const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
+
+taskAdder.addEventListener("submit", addTask);
+myTasks.addEventListener("click", toggleDone);
 
 function saveToLocalStorage() {
   localStorage.setItem("taskList", JSON.stringify(tasks));
 }
-
-taskAdder.addEventListener("submit", addTask);
-myTasks.addEventListener("click", toggleDone);
 
 function renderTask() {
   letTaskToRender = [];
@@ -23,22 +23,32 @@ function renderTask() {
                         </div>
                     </li>`;
   });
-
-  function addTask(event) {
-    event.preventDefault();
-    const task = {
-      textTask: taskInput.value,
-      done: false,
-    };
-    taskList.push(task);
-    saveToLocalStorage();
-    renderTask();
-    this.reset;
-  }
-
   myTasksContainer.innerHTML = tasksToRender.join("");
+}
 
+function addTask(event) {
+  event.preventDefault();
+  const task = {
+    textTask: taskInput.value,
+    done: false,
+  };
+  taskList.push(task);
+  saveToLocalStorage();
   renderTask();
+  this.reset;
+}
 
-  function toggleDone(e) {}
+function toggleDone(e) {
+  const myEl = e.target;
+  const mySel = myEl.parentElement;
+  if (myEl.className === "remove") {
+    let index = mySel.parentElement.dataset.index;
+    let temp = tasks.splice(index, 1);
+    console.log(temp);
+  } else {
+    myEl.classList.toggle("done");
+    tasks[mySel.dataset.index].done = !tasks[mySel.dataset.index].done;
+  }
+  saveToLocalStorage();
+  renderTask();
 }

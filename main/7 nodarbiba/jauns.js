c38f92fd05eee2461e685a972aef4d2b8dc41b17
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-14 20:30:50 +0300

    7md

--- main/7 nodarbiba/jauns.js
+++ main/7 nodarbiba/jauns.js
@@ -1,79 +1,64 @@
-// TODO app (dienas planotaju)
-// Nokopejiet šo mapi savas majas darba mape
-// Aplikacijas darbas principi var apskatit video appDemo.mp4
-
-/* Mums nepieciešams izviedot nelielu todo aplikaciju ar iespeju pievienot izdzest un atziment izdarito notikumu
- */
 const taskAdder = document.querySelector(".taskAdder");
 const myTasks = document.querySelector(".myTasks");
+
+//Use localStorage to store todos
 const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
-// gan kad ir dati, gan kad nav
 
-taskAdder.addEventListener("submit", addTask);
-myTasks.addEventListener("click", toggleDone);
-//otrs domāts, lai nomainītu statusu - spiežot uz krustiņu
-
-function renderTask() {
-  //lai izvadītu datus vajadzīgajā vietā
-  let html = tasks.map(function (data, i) {
-    //var arī ar forEach ciklu - šis ir map
-    let myClass = data.done ? "done" : "";
-
-    //ja true klasi definē par done,
-
-    return `<li data-index='${i}'>
-                        <div class="">
-                            ${data.textTask}<span class="remove">❌</span>
-                        </div>
-                    </li>`;
-  });
-  myTasks.innerHTML = html.join("");
+// Check if there is any todo in localStorage and add them to the list
+if (tasks) {
+  tasks.forEach((tasks) => addTask(tasks));
 }
 
-function addTask(e) {
+// Add todo on enter key press
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const textTask = this.querySelector("[name=task]").value;
-  //[name=task] vietā var definēt vai id, vai klasi,ja būtu definēts html
+  addTask();
+});
 
-  const task = {
-    textTask,
-    done: false,
-  };
-  tasks.push(task);
-  saveToLocalStorage();
-  renderTask();
-
-  this.reset();
-  // restartē elementu
-}
-
-function saveToLocalStorage() {
-  localStorage.setItem("taskList", JSON.stringify(tasks));
-}
-
-function toggleDone(e) {
-  const myxEl = e.targets;
-  const mySel = myEl.parentElement;
-
-  if (myEl.className === "remove") {
-    //lai pārliecinātos, kas nospiests - atzīmēts kā done- vai spiests uz el vai krustiņu
-
-    let index = mySel.parenetElement.dataset.index;
-
-    let = temp = tasks.splice(index, 1);
-    //lai saprastu,kāds elements nospiests
-
-    console.log(temp);
-    //temp saglabā masīvu ar izejas vērtību
-  } else {
-    myEl.classList.toggle("done");
-    //ja nozpiež done nevis remove
-    tasks[mySel.dataset.index].done = !tasks[mySel.dataset.index].done;
-    //aizvieto if :
-    //    if (tasks[4].done === true) {
-    //ja ceturtais el === ar true\
-    //        tasks[4].done===false...
+// Create a function to add todo to the list
+function addTask(task) {
+  let taskText = input.value;
+  if (task) {
+    taskText = task.text;
   }
-  saveToLocalStorage();
-  renderTask();
+  if (taskText) {
+    // Create a li for adding the todo list
+    const taskEl = document.createElement("li");
+    if (task && task.completed) {
+      //Add completed class to the li
+      taskEl.classList.add("completed");
+    }
+    taskEl.innerText = taskText;
+    //Add click event listener to the li to toggle the completed class
+    taskEl.addEventListener("click", () => {
+      taskEl.classList.toggle("completed");
+      addtask();
+    });
+    //Add delete event listener to the li to delete the li
+    taskEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      taskEl.remove();
+      addTask();
+    });
+    //Add the li to the existng list
+    tasksList.appendChild(taskEl);
+    input.value = "";
+    //Update the localStorage
+    addTask();
+  }
+}
+
+function adTask() {
+  //Get all the todos from the list
+  taskEl = document.querySelectorAll("li");
+  const tasks = [];
+  //Loop through the todos and add them to the todos array
+  taskEl.forEach((taskkEl) => {
+    tasks.push({
+      text: taskEl.innerText,
+      completed: taskEl.classList.contains("completed"),
+    });
+  });
+  //Store the todos in the localStorage and stringify it
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }

f3698b6a31c1bc7e5d26bc006cb89a9309e833e3
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-15 19:45:57 +0300

    9

--- main/9 nodarbiba/app.js
+++ main/9 nodarbiba/app.js
@@ -1,24 +1,23 @@
-const dogSelector = document.getElementById('dogSelector');
-const dogImgSelector = document.getElementById('dogImg');
+const dogSelector = document.getElementById("dogSelector");
+const dogImgSelector = document.getElementById("dogImg");
 
 function renderDogData(data) {
-    data.forEach(breed => {
-        dogSelector.innerHTML += '<option value=${breed.value}>${breed.name}</option>'
-    })
+  data.forEach((breed) => {
+    dogSelector.innerHTML +=
+      "<option value=${breed.value}>${breed.name}</option>";
+  });
 }
 
-functiom renderDogImg(data) {
-dogImgSelector.inner.HTML = <img src=
+function renderDogImg(data) {
+  dogImgSelector.inner.HTML = `<img src='${data.message}'>`;
 }
 
 function getDogImg() {
-    const url = 'https://dog.ceo/api/${dogSelectorValue}/images/random/'
-    fetch(url)
-        .then(response => response.json())
-        .then(data => renderDogData(data))
-    .catch.error = alert (error)
+  const url = "https://dog.ceo/api/${dogSelectorValue}/images/random/";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderDogData(data)).catch.error = alert(error);
 }
 
-renderDogData(data)
-dogSelector.addEventListener('change', getDogImg)
-
+renderDogData(data);
+dogSelector.addEventListener("change", getDogImg);

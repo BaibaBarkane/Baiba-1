de19c0940ca5a1be8272d4a5f7009ab607284e60
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-20 21:05:41 +0300

    g

--- main/10 nodarbiba/js/main.js
+++ main/10 nodarbiba/js/main.js
@@ -17,28 +17,31 @@ function convertion(val) {
 
 // fetch metode
 
-tn.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
-          .then(res => res.json())
-  .then(data => {
+tn.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      inputval.value +
+      "&appid=" +
+      apik
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      let nameval = data["name"];
+      let descrip = data["weather"]["0"]["description"];
+      let tempature = data["main"]["temp"];
+      let wndspd = data["wind"]["speed"];
+      let rise = data["sun"]["rise"];
+      let set = data["sun"]["set"];
 
-    let nameval = data['name'];
-    let descrip = data['weather']['0']['description'];
-    let tempature = data['main']['temp'];
-    let wndspd = data['wind']['speed'];
-    let rise = data['sun']['rise'];
-    let set = data['sun']['set'];
-    
-
-    city.innerHTML = `Weather of <span>${nameval}<span>`
-            temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
-            description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-    wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
-    sunRise.innerHTML = '<span.${rise}</span>'
-    sunSet.innerHTML= '<span.${set}</span>'
-
-        })
-
+      city.innerHTML = `Weather of <span>${nameval}<span>`;
+      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
+      description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
+      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
+      sunRise.innerHTML = "<span.${rise}</span>";
+      sunSet.innerHTML = "<span.${set}</span>";
+    })
+    .catch((err) => alert("You entered Wrong city name"));
+});
 
 // on btn click funkcija
 // addEventListener

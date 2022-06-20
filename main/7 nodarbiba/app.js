a0f5bbc72d4f3f7a59a0a4048cea883eb2859eb5
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-20 21:26:26 +0300

    g

--- main/7 nodarbiba/app.js
+++ main/7 nodarbiba/app.js
@@ -1,41 +1,52 @@
-const taskAdder = document.querySelector(".taskAdder"); //pievieno elementus
-const myTasks = document.querySelector(".myTasks"); //Šeit izvadam datus no Localstorage
-const tasks = JSON.parse(localStorage.getItem("taskList")) || []; //funkcija, kas noglabās in storage
+const taskAdder = document.querySelector(".taskAdder");
+const myTasks = document.querySelector(".myTasks");
+const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
 
 taskAdder.addEventListener("submit", addTask);
 myTasks.addEventListener("click", toggleDone);
 
 renderTask();
 
+function saveToLocalStorage(task) {
+  tasks.push(task);
+  localStorage.setItem("taskList", JSON.stringify(tasks));
+}
+
 function addTask(e) {
   e.preventDefault();
   const textTask = this.querySelector("[name=task]").value;
   const task = {
-    textTask,
+    textTask: newTask,
     done: false,
   };
-
-  tasks.push(task);
-  saveToLocalStorage();
-  renderTask();
-  this.reset();
+  saveToLocalStorage(task);
 }
+
+tasks.push(task);
+saveToLocalStorage();
+renderTask();
+this.reset();
 
 function renderTask() {
-  let html = tasks.map(function (data, i) {
-    let myClass = data.done ? "done" : "";
-    return `<li data-index='${i}'>
-                <div class='${myClass}'>
-                 ${data.textTask}<span class="remove">❌</span>
-                 </div>
-             </li>`;
+  let tasksToRender = [];
+  tasks.forEach((item, i) => {
+    if (item.done === true) {
+      let eachTask = `<li data-index='${i}'>
+                            <div class="done">
+                            ${item.textTask}<span class="remove" id="delete"> ❌ </span>
+                            </div>
+                            </li>`;
+      tasksToRender.push(eachTask);
+    } else {
+      let eachTask = `<li data-index='${i}'>
+                            <div class="todo">
+                            ${item.textTask}<span class="remove" id="delete"> ❌ </span>
+                            </div>
+                            </li>`;
+      tasksToRender.push(eachTask);
+    }
   });
-
-  myTasks.innerHTML = html.join("");
-}
-
-function saveToLocalStorage() {
-  localStorage.setItem("taskList", JSON.stringify(tasks));
+  taskContainer.innerHTML = tasksToRender.join("");
 }
 
 function toggleDone(e) {

782e135aa57351339c354b6f1232862da3fb698d
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-28 22:37:59 +0300

    Gd

--- main/Galadarbs/js/main.js
+++ main/Galadarbs/js/main.js
@@ -1,4 +1,4 @@
-// Tiek definēti mainīgie
+// Definējam mainīgos
 
 let inputval = document.querySelector("#cityinput");
 let btn = document.querySelector("#add");
@@ -11,13 +11,13 @@ let sunSet = document.querySelector("#sunset");
 
 let apik = "3045dd712ffe6e702e3245525ac7fa38";
 
-//funkcija, kas pārvērš Kelvinus Celsija grādos
+//Funkcija, kas pārvērš Kelvinus Celsija grādos
 
 function toCelcius(val) {
   return (val - 273).toFixed(2);
 }
 
-// Pēc Submit taustiņa nospiešanas
+// Pēc Submit taustiņa nospiešanas ar fetch metodi iegūstam informāciju no api
 
 btn.addEventListener("click", function () {
   fetch(
@@ -28,7 +28,7 @@ btn.addEventListener("click", function () {
   )
     .then((res) => res.json())
     .then((data) => {
-      //iegūstam informāciju no api
+      //Iegūto informāciju saglabājam mainīgajos
 
       let nameval = data["name"];
       let descrip = data["weather"]["0"]["description"];
@@ -37,7 +37,7 @@ btn.addEventListener("click", function () {
       let rise = data["sys"]["sunrise"];
       let set = data["sys"]["sunset"];
 
-      //Izvadām iegūto informāciju
+      //Izmantojot innerHTML izvadām iegūto informāciju
 
       city.innerHTML = `Weather of <span>${nameval}<span>`;
       temp.innerHTML = `Temperature: <span>${toCelsius(temperature)} C</span>`;
@@ -47,5 +47,8 @@ btn.addEventListener("click", function () {
       sunRise.innerHTML = `Sunrise: <span.${rise}</span>`;
       sunSet.innerHTML = `Sunset: <span.${set}</span>`;
     })
+
+    //Ja pieprasītā pilsēta nav atrodama, izvadām kļūdas paziņojumu
+
     .catch((err) => alert("You entered wrong city name"));
 });

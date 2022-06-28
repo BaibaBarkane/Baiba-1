4431c664d4dda41bf84b9d98f1656e87aba240a8
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-28 23:10:19 +0300

    Gd

--- main/Galadarbs/m.js
+++ main/Galadarbs/m.js
@@ -1,16 +1,16 @@
 //Now we need to determine the constant of one of the id functions. Because no html function can be used directly in JavaScript.
-var inputval = document.querySelector("#cityinput");
-var btn = document.querySelector("#add");
-var city = document.querySelector("#cityoutput");
-var descrip = document.querySelector("#description");
-var temp = document.querySelector("#temp");
-var wind = document.querySelector("#wind");
+let inputval = document.querySelector("#cityinput");
+let btn = document.querySelector("#add");
+let city = document.querySelector("#cityoutput");
+let descrip = document.querySelector("#description");
+let temp = document.querySelector("#temp");
+let wind = document.querySelector("#wind");
 
 apik = "3045dd712ffe6e702e3245525ac7fa38";
 
 //kelvin to celcious. 1 Kelvin is equal to -272.15 Celsius.
 
-function convertion(val) {
+function toCelsius(val) {
   return (val - 273).toFixed(2);
 }
 //Now we have to collect all the information with the help of fetch method
@@ -33,11 +33,11 @@ btn.addEventListener("click", function () {
 
       var nameval = data["name"];
       var descrip = data["weather"]["0"]["description"];
-      var tempature = data["main"]["temp"];
+      var temperature = data["main"]["temp"];
       var wndspd = data["wind"]["speed"];
       //Now with the help of innerHTML you have to make arrangements to display all the information in the webpage.
       city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
+      temp.innerHTML = `Temperature: <span>${toCelsius(temperature)} C</span>`;
       description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
       wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
     })

f960e273ddc956e9c3f24244f56f48bff4637d3b
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-28 21:57:12 +0300

    Gd

--- main/Galadarbs/js/main.js
+++ main/Galadarbs/js/main.js
@@ -2,7 +2,7 @@
 
 let inputval = document.getElementById("cityinput");
 let btn = document.getElementById("add");
-let city = "";
+let city = document.getElementById("cityoutput");
 let descrip = document.getElementById("description");
 let temp = document.getElementById("temp");
 let wind = document.getElementById("wind");
@@ -17,7 +17,7 @@ function toCelcius(val) {
   return (val - 273).toFixed(2);
 }
 
-// Pēc Submit nospiešanas ar fetch metodi
+// Pēc SZubmit taustiņa nospiešanas
 
 btn.addEventListener("click", function () {
   fetch(
@@ -32,15 +32,14 @@ btn.addEventListener("click", function () {
 
       let nameval = data["name"];
       let descrip = data["weather"]["0"]["description"];
-      let temperature = data["main"]["temp"];
+      let tempature = data["main"]["temp"];
       let wndspd = data["wind"]["speed"];
       let rise = data["sys"]["sunrise"];
       let set = data["sys"]["sunset"];
 
       //Izvadām iegūto informāciju
-
       city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${toCelsius(temperature)} C</span>`;
+      temp.innerHTML = `Temperature: <span>${toCelsius(tempature)} C</span>`;
       description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
       wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
       sunRise.innerHTML = `Sun rise: <span.${rise}</span>`;

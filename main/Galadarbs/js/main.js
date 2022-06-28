b32683ac7b46bf6bcdfedfe26af3bfb8bf40e0c3
BaibaBarkane <baiba.barkane@gmail.com>
2022-06-28 22:14:28 +0300

    Gd

--- main/Galadarbs/js/main.js
+++ main/Galadarbs/js/main.js
@@ -4,12 +4,12 @@ let inputval = document.querySelector("#cityinput");
 let btn = document.querySelector("#add");
 let city = document.querySelector("#cityoutput");
 let descrip = document.querySelector("#description");
-let temp = document.getElementById("temp");
-let wind = document.getElementById("wind");
-let sunRise = document.getElementById("sunrise");
-let sunSet = document.getElementById("sunset");
+let temp = document.getElementById("#temp");
+let wind = document.getElementById("#wind");
+let sunRise = document.getElementById("#sunrise");
+let sunSet = document.getElementById("#sunset");
 
-apik = "3045dd712ffe6e702e3245525ac7fa38";
+let apik = "3045dd712ffe6e702e3245525ac7fa38";
 
 //funkcija, kas pārvērš Kelvinus Celsija grādos
 
@@ -17,7 +17,7 @@ function toCelcius(val) {
   return (val - 273).toFixed(2);
 }
 
-// Pēc SZubmit taustiņa nospiešanas
+// Pēc Submit taustiņa nospiešanas
 
 btn.addEventListener("click", function () {
   fetch(
@@ -32,14 +32,15 @@ btn.addEventListener("click", function () {
 
       let nameval = data["name"];
       let descrip = data["weather"]["0"]["description"];
-      let tempature = data["main"]["temp"];
+      let temperature = data["main"]["temp"];
       let wndspd = data["wind"]["speed"];
       let rise = data["sys"]["sunrise"];
       let set = data["sys"]["sunset"];
 
       //Izvadām iegūto informāciju
       city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${toCelsius(tempature)} C</span>`;
+      temp.innerHTML = `Temperature: <span>${toCelsius(temperature)} C</span>`;
+      temp.innerHTML = "Temperature: ${(data.main.temp)} C";
       description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
       wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
       sunRise.innerHTML = `Sun rise: <span.${rise}</span>`;
